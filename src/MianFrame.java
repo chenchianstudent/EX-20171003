fd0844d8fcbdbd32e32cad73d624102f93f1cc3c
chenchianstudent <105021007@live.asia.edu.tw>
2017-10-03 12:12:39 +0800

    02

--- src/MianFrame.java
+++ src/MianFrame.java
@@ -5,12 +5,17 @@ import java.awt.event.ActionListener;
 
 public class MianFrame extends JFrame {
     private Container cp;
-    private JButton jb1 = new JButton("++");
-    private JButton jb2 = new JButton("--");
-    private JButton jb3 = new JButton("test");
+    private JPanel jpnc = new JPanel(new GridLayout(6, 1));
+    private JButton jb1 = new JButton("example");
+    private JButton jb2 = new JButton("++");
+    private JButton jb3 = new JButton("清除");
     private JButton jb4 = new JButton("exit");
-    private JLabel lb1 = new JLabel("0");
-    private int count = 0;
+    private JLabel lb1 = new JLabel("key");
+    private JTextField jtfkey = new JFormattedTextField("3");
+    private JTextArea jtaL = new JTextArea();
+    private JTextArea jtaR = new JTextArea();
+    private JScrollPane jspL = new JScrollPane(jtaL);
+    private JScrollPane jspR = new JScrollPane(jtaR);
 
     public MianFrame() {
         init();
@@ -20,31 +25,43 @@ public class MianFrame extends JFrame {
         this.setBounds(100, 100, 500, 400);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         cp = this.getContentPane();
-        cp.setLayout(new GridLayout(5, 1, 3, 1));
-        cp.add(jb1);
-        cp.add(jb2);
-        cp.add(lb1);
-        cp.add(jb3);
-        cp.add(jb4);
+        cp.setLayout(new BorderLayout(3, 3));
+        jtaL.setPreferredSize(new Dimension(200, 400));
+        jtaR.setPreferredSize(new Dimension(200, 400));
+        jtaR.setLineWrap(true);
+        jtaL.setLineWrap(true);
+        cp.add(jtaL, BorderLayout.WEST);
+        cp.add(jtaR, BorderLayout.EAST);
+        jpnc.add(jb1);
+        jpnc.add(jb2);
+        jpnc.add(lb1);
+        jpnc.add(jtfkey);
+        jpnc.add(jb3);
+        jpnc.add(jb4);
         lb1.setHorizontalAlignment(JLabel.CENTER);
+        cp.add(jpnc, BorderLayout.CENTER);
         jb1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                count++;
-                lb1.setText(Integer.toString(count));
+                jtaL.setText("poipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoipoi");
             }
         });
         jb2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                count--;
-                lb1.setText(Integer.toString(count));
+                char data[] = jtaL.getText().toCharArray();
+                int len = data.length;
+                for (int i = 0; i < len; i++) {
+                    data[i] += Integer.parseInt(jtfkey.getText());
+                }
+                jtaR.setText(new String(data));
             }
         });
         jb3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                jtaR.setText("");
+                jtaL.setText("");
             }
         });
         jb4.addActionListener(new ActionListener() {
